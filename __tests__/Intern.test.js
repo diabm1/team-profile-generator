8a124241e0c7164de93763af26dbdb7bc032ab79
diabm1 <diabm1855@gmail.com>
2022-06-28 15:44:42 -0400

    finished adding rest of and then started working on index.html

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("Can set intern via constructor", () => {
+  const testValue = "OSU";
+  const e = new Intern("Tom", 1, "a@b.c", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test('getRole() should return "Intern"', () => {
+  const testValue = "Intern";
+  const e = new Intern("Tom", 1, "a@b.c", "OSU");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "OSU";
+  const e = new Intern("Tom", 1, "a@b.c", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
